5bdf0760618e6688efd7e50a80e6228c6f5c9bc8
EmaConor <159412831+EmaConor@users.noreply.github.com>
2025-12-13 16:38:18 -0500

    feat: implement database schema and migrations

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,8 @@
 import { defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 import { nanoid } from 'nanoid'
+import { db } from '@/db/index'
+import { short_url } from '@/db/schema';
 
 export const server = {
     setShortUrl: defineAction({
@@ -11,8 +13,13 @@ export const server = {
         handler: async ({ url }) => {
             try {
                 console.log({url});
-                const shortCode = nanoid(8);
+                const shortCode = nanoid(6);
                 console.log({shortCode})
+                
+                await db.insert(short_url).values({
+                    full_url: url,
+                    short_code: shortCode
+                })
 
                 return {
                     success: true,
@@ -20,8 +27,8 @@ export const server = {
                     message: 'URL encontrada con exito',
                 };
             } catch (error) {
-            console.error("Error guardando en la base de datos: ", error);
-            throw new Error("No se pudo guardar la URL. intenta de nuevo")
+                console.error("Error guardando en la base de datos: ", error);
+                throw new Error("No se pudo guardar la URL. intenta de nuevo")
             }
         }
     })
---
+++ src/db/index.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+
+const connectionString = import.meta.env.DATABASE_URL
+
+if (!connectionString) {
+    throw new Error("🔴 ERROR CRÍTICO: No se encontró la variable DATABASE_URL. Revisa tu archivo .env");
+}
+console.log("🟢 Conectando a la base de datos..."); 
+
+const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client, { schema });
