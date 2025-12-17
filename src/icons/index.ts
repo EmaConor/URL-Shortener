8ff56ffbb6558edf314f2518aeb6838db4ec5f2a
EmaConor <159412831+EmaConor@users.noreply.github.com>
2025-12-16 21:36:05 -0500

    feat: add SVG icon library for UI components

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+
+export { default as Email } from './Email.svg';
+export { default as Key } from './Key.svg';
+export { default as GitHub } from './GitHub.svg';
+export { default as ArroyBackUp } from './ArroyBackUp.svg';
+export { default as User } from './User.svg';
